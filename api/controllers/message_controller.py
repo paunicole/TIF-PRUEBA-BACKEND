aa61f3e7035d8f9dc25e32f3171a8b8c9febccc9
paunicole <nicolecardozogomez@gmail.com>
2023-09-24 16:52:05 -0300

    Mejorado metodo get de mensajes

--- api/controllers/message_controller.py
+++ api/controllers/message_controller.py
@@ -11,13 +11,26 @@ class MessageController:
         )).serialize(), 200
     
     @classmethod
-    def get_messages(self):
-        print("LLEGO A GET_MESSAGES")
-        message_objects = Message.get_messages()
+    def get_messages(cls):
         messages = []
-        for message in message_objects:
-            messages.append(message.serialize())
-        return messages, 200
+        if request.args.get('channel_id'):
+            message_obj = Message(channel_id=request.args.get('channel_id'))
+            print("MESSAGE OBJ:", message_obj.channel_id, message_obj.message, message_obj.channel_id)
+            messages = Message.get_messages(message_obj)
+        else:
+            print("SEGUNDO GET")
+            messages = Message.get_messages()
+        return [message.serialize() for message in messages], 200
+
+    # @classmethod
+    # def get_messages(self):
+    #     print("LLEGO A GET_MESSAGES")
+    #     message_objects = Message.get_messages()
+    #     print("DESPUES DE GET_MESSAGES")
+    #     messages = []
+    #     for message in message_objects:
+    #         messages.append(message.serialize())
+    #     return messages, 200
 
     @classmethod
     def create_message(self):
